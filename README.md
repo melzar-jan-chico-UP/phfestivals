38590e443b43fa84f4bb194d372e91568498e8dd
Melzar Jan Chico <mechico@up.edu.ph>
2023-06-19 12:51:00 +0800

    another commit hayst

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-# Responsive Travel Website
-## [Watch it on youtube](https://youtu.be/cgV2tN8gxCg)
-### Responsive Travel Website
+# Responsive Landing Page Islands Travel 🏝️
+## [Watch it on youtube](https://youtu.be/6GBVaHH_6bQ)
+### Responsive Landing Page Islands Travel
+Beautiful Responsive Landing Page Islands Travel 🏝️ Using Html, Css and JavaScript. It contains a header and a home that shows various sections of different islands.
+Don't forget to join the channel for more videos like this. [Bedimcode](https://www.youtube.com/c/Bedimcode)
 
-- Responsive Travel Website Design Using HTML CSS & JavaScript
-- Contains animations when scrolling.
-- Smooth scrolling in each section.
-- Developed first with the Mobile First methodology, then for desktop.
-- Compatible with all mobile devices and with a beautiful and pleasant user interface.
-
-💙 Join the channel to see more videos like this. [Bedimcode](https://www.youtube.com/@Bedimcode)
-
-![preview img](/preview.png)
+![Landing Page Travel](/preview.png)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,61 @@
-/*=============== SHOW MENU ===============*/
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/*==================== SWIPER JS ====================*/
+let galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 0,
+    slidesPerView: 0,
+})
+
+let galleryTop = new Swiper('.gallery-top', {
+    effect: 'fade',
+    loop: true,
+
+    thumbs: {
+      swiper: galleryThumbs
+    }
+})
 
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*==================== POPUP ====================*/
+const btnOpenVideo = document.querySelectorAll('.islands__video-content')
+const islandsPopup = document.getElementById('popup')
 
+function poPup(){
+    islandsPopup.classList.add('show-popup')
+}
+btnOpenVideo.forEach(b => b.addEventListener('click', poPup))
 
-/*=============== ADD BLUR TO HEADER ===============*/
+const btnCloseVideo = document.getElementById('popup-close')
 
+btnCloseVideo.addEventListener('click', ()=> {
+    islandsPopup.classList.remove('show-popup')
+})
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*==================== GSAP ANIMATION ====================*/
+const controlImg = document.querySelectorAll('.controls__img')
 
+function scrollAnimation(){
+    gsap.from('.islands__subtitle', {opacity: 0, duration: .2, delay: .2, y: -20})
+    gsap.from('.islands__title', {opacity: 0, duration: .3, delay: .3, y: -20})
+    gsap.from('.islands__description', {opacity: 0, duration: .4, delay: .4, y: -20})
+    gsap.from('.islands__button', {opacity: 0, duration: .5, delay: .5, y: -20})
+    gsap.from('.islands__video-content', {opacity: 0, duration: .6, delay: .6, y: -20})
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+    islandsPopup.classList.remove('show-popup')
+}
 
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+controlImg.forEach(c => c.addEventListener('click', scrollAnimation))
